970d79692da92f5fbfd66246584e77b58601de4b
Coeur <alexxisgobrin06@gmail.com>
2024-11-18 00:25:49 -0600

    Init

---
+++ script/localeHandler.js
@@ -0,0 +1,14 @@
+﻿// document.addEventListener('DOMContentLoaded', () => {
+//    const locale = document.querySelector('.localeSelector'),
+//    navHome = document.querySelector('.navItem1'),
+//    navShop = document.querySelector('.navItem2'),
+//    navCommunity = document.querySelector('.navItem3'),
+//    navContact = document.querySelector('.navItem4'),
+//    navLogin = document.querySelector('.navItem5');
+//
+//    locale.forEach((item) => {
+//       item.addEventListener('click', () => {
+//
+//       })
+//    })
+// });
